ce71f9288bd128d496c7853203f6bd91400e3d3d
Craig Carnell <craig_carnell@hotmail.com>
2014-03-10 09:32:13 +0000

    update hhvm build depends for ubuntu 13.10

--- manifests/install/prerequisites.pp
+++ manifests/install/prerequisites.pp
@@ -8,13 +8,14 @@ class hhvm::install::prerequisites(
   
   $ubuntu_packages = [
     
-		  'git', 'cmake', 'g++', 'libboost-dev', 'libmysqlclient-dev', 'libxml2-dev',
-		  'libmcrypt-dev', 'libicu-dev', 'binutils-dev', 'libcap-dev', 'libgd2-xpm-dev',
-		  'zlib1g-dev', 'libtbb-dev', 'libonig-dev', 'libpcre3-dev', 'autoconf', 'libtool', 'libcurl4-openssl-dev',
-		  'libboost-system-dev', 'libboost-program-options-dev', 'libboost-filesystem-dev', 'libboost-thread-dev',
-		  'libreadline-dev', 'libncurses-dev', 'libmemcached-dev', 'libbz2-dev', 'libc-client2007e-dev',
-		  'libgoogle-perftools-dev', 'libelf-dev', 'libdwarf-dev', 'libboost-regex-dev', 'libgoogle-glog-dev',
-		  'libjemalloc-dev'
+      'cmake',  'g++', 'git',
+      'libboost-dev', 'libboost-filesystem-dev', 'libboost-program-options-dev', 'libboost-regex-dev',
+      'libboost-system-dev', 'libboost-thread-dev', 'libbz2-dev', 'libc-client-dev',
+      'libc-client2007e-dev', 'libcap-dev', 'libcurl4-openssl-dev', 'libdwarf-dev', 'libelf-dev',
+      'libexpat-dev', 'libgd2-xpm-dev', 'libgoogle-glog-dev', 'libgoogle-perftools-dev', 'libicu-dev', 
+      'libjemalloc-dev', 'libmcrypt-dev', 'libmemcached-dev', 'libmysqlclient-dev', 'libncurses-dev',
+      'libonig-dev', 'libpcre3-dev', 'libreadline-dev', 'libtbb-dev', 'libtool', 'libxml2-dev', 'zlib1g-dev', 'libevent-dev',
+      'libmagickwand-dev', 'libxslt1-dev'
   ]
   
   package { $ubuntu_packages:
