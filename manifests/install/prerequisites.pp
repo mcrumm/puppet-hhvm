e5fa6ed8521ebba248b9b4cae422a943e416c3b4
Craig Carnell <craig_carnell@hotmail.com>
2014-03-21 11:40:50 +0000

    test for build-essential already defined

--- manifests/install/prerequisites.pp
+++ manifests/install/prerequisites.pp
@@ -3,12 +3,18 @@
 # This module manages HHVM prerequisites installation
 class hhvm::install::prerequisites(
 ) {
+  require gcc
+  
   # build-essential already defined in gcc
-  #require gcc
+  if !defined(Package['build-essential']) {
+	 package { 'build-essential':
+	   ensure => installed,
+	 }
+  }
   
   $ubuntu_packages = [
     
-      'autoconf', 'automake', 'binutils-dev', 'build-essential', 'cmake', 'g++', 'git',
+      'autoconf', 'automake', 'binutils-dev', 'cmake', 'g++', 'git',
 		  'libboost-dev', 'libboost-filesystem-dev', 'libboost-program-options-dev', 'libboost-regex-dev',
 		  'libboost-system-dev', 'libboost-thread-dev', 'libbz2-dev', 'libc-client-dev',
 		  'libc-client2007e-dev', 'libcap-dev', 'libcurl4-openssl-dev', 'libdwarf-dev libelf-dev',
