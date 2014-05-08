e5ae94c119cb851d17889b6a9ce57e50f389b8d3
Craig Carnell <craig_carnell@hotmail.com>
2014-05-08 11:44:09 +0100

    update build to use recursive submodules

--- files/build-hhvm.sh
+++ files/build-hhvm.sh
@@ -1,20 +1,7 @@
 cd /usr/local/src/hiphop-php
 export CMAKE_PREFIX_PATH=`pwd`
 cd /usr/local/src/hiphop-php/hhvm
-git submodule init
-  
-# compile libevent
-#cd /usr/local/src/hiphop-php/libevent
-#cat ../hhvm/hphp/third_party/libevent-1.4.14.fb-changes.diff | patch -p1
-#./autogen.sh
-#./configure --prefix=/usr/local/src/hiphop-php
-#make
-#make install
-#cd ..
-  
-# build hiphop
-#cd hhvm
-git submodule update --init
-#export HPHP_HOME=`pwd`
+git submodule update --init --recursive
 cmake .
 make
+make install
