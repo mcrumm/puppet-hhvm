b94dc6abb170090f24bdf65c4a89d74697861bd1
Craig Carnell <craig_carnell@hotmail.com>
2014-05-28 17:43:56 +0100

    make use of ini files instead of config.hdf going forward

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,7 +10,6 @@
 class hhvm(
   $sourceroot = "/var/www/vhosts/magento-capistrano/current",
   $adminpassword = 'UyPutcu9',
-  $port = 9000
 ) {
     class { 'hhvm::config': }
 	  class { 'hhvm::install::package': }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,4 +1,10 @@
 class hhvm::params {
 
   $hhvm_user  = $operatingsystem ? { centos => 'nginx', ubuntu => 'www-data', default => 'nginx' }
+  $jit_repo = "/tmp/.hhbc.hhvm"
+  $port = 9000
+  $socket = "/var/run/hhvm/hhvm.sock"
+  $pid = "/var/run/hhvm/hhvm.pid"
+  $error_log = "/var/log/hhvm/error.log"
+  $timezone = "Europe/London"
 }
