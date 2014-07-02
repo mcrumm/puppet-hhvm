26f5c201c52ca8afa75358b8317344680ea7ce7c
Craig Carnell <craig_carnell@hotmail.com>
2014-07-02 16:29:43 +0100

    update build depends

--- manifests/install/prerequisites.pp
+++ manifests/install/prerequisites.pp
@@ -31,15 +31,17 @@ class hhvm::install::prerequisites(
   # We are only targeting 14.04 from now on
   $ubuntu_packages = [
   
-		  'autoconf', 'automake', 'binutils-dev', 'cmake', 'g++',
+      'autoconf', 'automake', 'binutils-dev', 'cmake', 'g++', 
 		  'libboost-dev', 'libboost-filesystem-dev', 'libboost-program-options-dev', 'libboost-regex-dev',
 		  'libboost-system-dev', 'libboost-thread-dev', 'libbz2-dev', 'libc-client-dev', 'libldap2-dev',
 		  'libc-client2007e-dev', 'libcap-dev', 'libcurl4-openssl-dev', 'libdwarf-dev', 'libelf-dev',
-		  'libexpat-dev', 'libgd2-xpm-dev', 'libgoogle-glog-dev', 'libgoogle-perftools-dev', 'libicu-dev',
+		  'libexpat-dev libgd2-xpm-dev', 'libgoogle-glog-dev', 'libgoogle-perftools-dev', 'libicu-dev',
 		  'libjemalloc-dev', 'libmcrypt-dev', 'libmemcached-dev', 'libmysqlclient-dev', 'libncurses-dev',
-		  'libonig-dev', 'libreadline-dev', 'libtbb-dev', 'libtool', 'libxml2-dev', 'zlib1g-dev', 'libevent-dev',
-		  'libmagickwand-dev', 'libinotifytools0-dev', 'libiconv-hook-dev', 'libedit-dev', 'libiberty-dev', 'libxslt1-dev', 'ocaml-native-compilers'
-
+      'libonig-dev', 'libreadline-dev', 'libtbb-dev', 'libtool', 'libxml2-dev', 'zlib1g-dev',
+		  'libevent-dev', 'libmagickwand-dev', 'libinotifytools0-dev', 'libiconv-hook-dev', 'libedit-dev',
+		  'libiberty-dev', 'libxslt1-dev',  'ocaml-native-compilers',
+		  'php5-imagick'
+  
   ]
 
   package { $ubuntu_packages:
