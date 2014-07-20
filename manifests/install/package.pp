b317f71b16bbecc1792a6083c73168d2fbc9a91c
Michael Crumm <mike@crumm.net>
2014-07-20 01:01:48 -0700

    Fixing apt::source

--- manifests/install/package.pp
+++ manifests/install/package.pp
@@ -29,10 +29,10 @@ class hhvm::install::package {
         apt::key { '1BE7A449': url => 'http://dl.hhvm.com/conf/hhvm.gpg.key' }
         
         apt::source { "hhvm":
-            url        => "http://dl.hhvm.com/ubuntu/",
-            distro     => 'trusty',
-            repository => 'main',
-            require    => Apt::Key["1BE7A449"]
+            location => "http://dl.hhvm.com/ubuntu/",
+            release  => 'trusty',
+            repos    => 'main',
+            require  => Apt::Key["1BE7A449"]
         }
 
         package { $hhvm_package_name:
